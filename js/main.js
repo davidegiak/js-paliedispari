3abdece5fcb24935ffcd497f15666731b82dd431
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-29 18:26:54 +0200

    riuscito

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-// Chiedere all’utente di inserire una parola.
-let word = prompt("scrivi una parola")
 // Creare una funzione per capire se la parola inserita è palindroma.
-function parola(letter) {
-    for (word.charAt = 0; word.charAt < word.length; word.charAt++) {
-        return console.log (word.charAt)
+
+function parola (string) {
+    
+    const stringSplit = string.split("");
+    
+    const stringReverse = stringSplit.reverse();
+    
+    const joinString = stringReverse.join("")
+    
+    
+    if (joinString == string) {
+        alert("PALINDROMA")
     }
+    else{
+        alert("LOOSER")
+    }
+    
+    return joinString
 }
 
-const pali = word.charAt (0) 
-console.log(pali)
+// Chiedere all’utente di inserire una parola.
+const parolaUser = prompt("scrivi qui la tua parola");
 
+const risultato = parola(parolaUser);
 
+console.log (risultato)
 // Sono accettate solo soluzioni che fanno uso del ciclo.
 // Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
-
