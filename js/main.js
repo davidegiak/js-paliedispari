be34e4ea9746ffb64e9f36bd3ab28205148565be
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-29 16:51:58 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Chiedere all’utente di inserire una parola.
+let word = prompt("scrivi una parola"),parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+function parola(letter) {
+    for (parola.charAt = 0; parola.charAt < parola.length; parola.charAt++) {
+        return parola.charAt
+    }
+}
+
+const pali = word.charAt (0) 
+console.log(pali)
+
+
+// Sono accettate solo soluzioni che fanno uso del ciclo.
+// Eventuali soluzioni più "comode" potete aggiungere come extra in un commit separato solo DOPO.
+
