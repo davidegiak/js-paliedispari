bebffe53aa51cbe5b9916de44f7f9c74620c9324
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-30 13:23:31 +0200

    fine

--- js/second.js
+++ js/second.js
@@ -1,47 +1,69 @@
-// Dichiariamo chi ha vinto.
-
 // L’utente sceglie su cosa puntare: pari o dispari.
 
 const pOd = prompt("pari o dispari ?");
+console.log(pOd)
 
 // L'utente inserisce un numero da 1 a 5.
 
 const oneToFive = prompt ("inserisci un numero da 1 a 5");
 const oneToFiveNumber = Number(oneToFive);
 console.log(oneToFiveNumber);
+
 // Il computer estrae un numero casuale sempre tra 1 e 5 (usando una funzione).
 
 function casualNumbers(max) {
     return Math.floor(Math.random() * max);
 }
-
-const casual = alert(Number(casualNumbers(6)));
+const casual = Number(casualNumbers(6));
+console.log(casual)
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 
-
 function add(num1, num2) {
-    return num1 + num2;
+    return somma = num1 + num2;
 }
-
-const somma = add(oneToFiveNumber, casual);
+somma = add(oneToFiveNumber, casual);
 console.log(somma);
 
-// function vincitore(addizione) {
-//     if (somma % 2 === 0) {
-//         const pari = alert("pari");
-//     }
-//     else {
-//         const dispari = alert("dispari");
-//     }
-// }
-// const vittoria = vincitore(somma)
+function vincitore(addizione) {
+    // if (somma % 2 === 0) {
+    //     const pari = alert("pari");
+    // }
+    // else {
+    //     const dispari = alert("dispari");
+    // }
+    // controllo addizione se è pari stampa un messaggio 
+    if (addizione % 2 == 0) {
+        alert ("pari")
+        return "pari"
+    }
+    // altrimenti un'altro
+    else {
+        alert ("dispari")
+        return "dispari"
+    }
+    
+}
+const vittoria = vincitore(somma);
+// Dichiariamo chi ha vinto.
 
-// if (pOd == pari || pOd == dispari ) {
-//     const haiVinto = alert("SEI UN MOSTRO")
-// }
 
+// if (pOd == pari) {
+//     const haiVinto = alert("SEI UN MOSTRO");
+// }
+// else if (pOd == dispari) {
+//     haiVinto = alert("SEI UN MOSTRO");
+// }
 // else {
-//     const haiPerso = alert("FAI RIDERE")
+//     const haiPerso = alert("FAI RIDERE");
 // }
+
+// controllo se la scelta dell'utente è uguale all'esito
+if (pOd == vittoria){
+    alert("hai vinto")
+}
+else {
+    alert ("hai perso")
+}
+
 
 
